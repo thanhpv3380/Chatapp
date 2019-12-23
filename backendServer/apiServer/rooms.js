5c4c91e864c1c4eddf9cee90509dd92e729b8702
Tien <ntien441@gmail.com>
2019-12-23 22:41:12 +0700

    fix getMessage api

--- backendServer/apiServer/rooms.js
+++ backendServer/apiServer/rooms.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-router = express.Router();
+    router = express.Router();
 
 const Room = require('../../backendServer/models/mongodb/Room');
 
@@ -7,7 +7,7 @@ router.route('/rooms')
     .post((req, res) => {
         var userId = req.body.userId;
         Room.findByUserId(userId, function (err, data) {
-            
+
             if (err) {
                 console.log(err)
                 res.json({
@@ -18,67 +18,69 @@ router.route('/rooms')
                 console.log(data)
                 res.json({
                     "status": true,
-                    "rooms": data.map((room) => {return{
-                        "roomId": room._id,
-                        "name": room.name, 
-                        //"members": room.members.map((member) => (member.userId)),
-                        "lastMessage": room.messages.length>0?room.messages.reduce((nearestMessage, cur)=>cur.time>nearestMessage.time?cur:nearestMessage):null
-                    }})
+                    "rooms": data.map((room) => {
+                        return {
+                            "roomId": room._id,
+                            "name": room.name,
+                            //"members": room.members.map((member) => (member.userId)),
+                            "lastMessage": room.messages.length > 0 ? room.messages.reduce((nearestMessage, cur) => cur.time > nearestMessage.time ? cur : nearestMessage) : null
+                        }
+                    })
                 })
             }
         })
     })
 
-    router.route('/getMessage')   
+router.route('/getMessage')
     .post((req, res) => {
         // var roomId = req.body.roomId;
         var time = new Date();
         var limit = req.body.limit;
         var roomId = req.body.roomId;
         console.log(time, limit, roomId)
-        Room.GetMessengerInRoom(roomId, function(err, messages){
-            if(err || messages == null){
+        Room.GetMessengerInRoom(roomId, function (err, messages) {
+            if (err || messages == null) {
                 res.json({
                     'status': false
                 })
-            }else{
+            } else {
                 // console.log(messages)
                 function compare(a, b) {
                     const messageA = a.time;
                     const messageB = b.time;
-                    
+
                     let comparison = 0;
                     if (messageA > messageB) {
-                      comparison = 1;
+                        comparison = 1;
                     } else if (messageA < messageB) {
-                      comparison = -1;
+                        comparison = -1;
                     }
                     return comparison;
-                   }
-                    
-                   messages.sort(compare);
-                   if(messages.length <= 10){
+                }
+
+                messages.sort(compare);
+                if (messages.length <= 10) {
                     res.json({
                         'status': true,
                         messages
                     })
-                   }else{
-                       var data = [];
-                       var length = messages.length -1;
-                       while(limit > 0){
-                           if(messages[length].time <= time){
-                               data.push(messages[length]);
-                               limit--;
-                           }
-                           length--;
-                       }
-                       res.json({
+                } else {
+                    var data = [];
+                    var length = messages.length - 1;
+                    while (limit > 0) {
+                        if (messages[length].time <= time) {
+                            data.push(messages[length]);
+                            limit--;
+                        }
+                        length--;
+                    }
+                    res.json({
                         'status': true,
-                        'messages': data.
+                        'messages': data
                     })
-                   }  
+                }
             }
         })
-    })  
+    })
 
 module.exports = router;
